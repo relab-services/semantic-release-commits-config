e034607d31ede0d7809633f505f241302408719c
Sergey Zwezdin <sergey@zwezdin.com>
2023-12-09 11:17:43 +0300

    feat(core): Initial version

---
+++ README.md
@@ -0,0 +1,31 @@
+# @sergeyzwezdin/semantic-release-config
+
+[**semantic-release**](https://github.com/semantic-release/semantic-release) shareable config.
+
+## Plugins
+
+This shareable configuration use the following plugins:
+
+- [`@semantic-release/commit-analyzer`](https://github.com/semantic-release/commit-analyzer)
+- [`@semantic-release/release-notes-generator`](https://github.com/semantic-release/release-notes-generator)
+- [`@semantic-release/github`](https://github.com/semantic-release/github)
+
+## Install
+
+```bash
+$ npm install --save-dev semantic-release @sergeyzwezdin/semantic-releasesemantic-release-config
+```
+
+## Usage
+
+The shareable config can be configured in the [**semantic-release** configuration file](https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration):
+
+```json
+{
+  "extends": "@sergeyzwezdin/semantic-release-config"
+}
+```
+
+## Configuration
+
+See each [plugin](#plugins) documentation for required installation and configuration steps.
---
+++ index.js
@@ -0,0 +1,89 @@
+module.exports = {
+  branches: ["master", "main"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        releaseRules: [
+          {
+            type: "feat",
+            release: "minor",
+          },
+          {
+            type: "fix",
+            release: "patch",
+          },
+          {
+            type: "chore",
+            release: "patch",
+          },
+          {
+            type: "refactor",
+            release: "patch",
+          },
+          {
+            type: "docs",
+            release: "patch",
+          },
+          {
+            type: "perf",
+            release: "patch",
+          },
+          {
+            type: "test",
+            release: "patch",
+          },
+          {
+            scope: "no-release",
+            release: false,
+          },
+        ],
+      },
+    ],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "conventionalcommits",
+        presetConfig: {
+          types: [
+            {
+              type: "feat",
+              section: "Features",
+            },
+            {
+              type: "fix",
+              section: "Fixes",
+            },
+            {
+              type: "chore",
+              section: "Other",
+            },
+            {
+              type: "docs",
+              section: "Documentation",
+            },
+            {
+              type: "style",
+              section: "UI",
+            },
+            {
+              type: "refactor",
+              section: "Refactoring",
+            },
+            {
+              type: "perf",
+              section: "Performance Improvements",
+            },
+            {
+              type: "test",
+              section: "Testing",
+            },
+          ],
+        },
+      },
+    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
+  ],
+};
